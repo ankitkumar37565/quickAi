2d470e2c9e45d2feac495a6dc63c2f7956e465be
ankit <ankitkumar37565@gmail.com>
2025-07-21 14:12:48 +0530

    extension name added

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
-import NavBar from '../components/NavBar'
-import Hero from '../components/Hero'
-import AiTools from '../components/AiTools'
-import Testimonial from '../components/Testimonial'
-import Plan from '../components/Plan'
-import Footer from '../components/Footer'
+import NavBar from '../components/NavBar.jsx'
+import Hero from '../components/Hero.jsx'
+import AiTools from '../components/AiTools.jsx'
+import Testimonial from '../components/Testimonial.jsx'
+import Plan from '../components/Plan.jsx'
+import Footer from '../components/Footer.jsx'
 
 const Home = () => {
   return (
     <>
-    <NavBar />
-    <Hero />
-    <AiTools />
-    <Testimonial />
-    <Plan />
-    <Footer />
+      <NavBar />
+      <Hero />
+      <AiTools />
+      <Testimonial />
+      <Plan />
+      <Footer />
     </>
   )
 }
